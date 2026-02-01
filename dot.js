8078164c48b3c30811929f72a8a304519f6f40cd
coder-utshab <ghoshutshob470@gmail.com>
2026-02-02 01:55:52 +0530

    recup

---
+++ dot.js
@@ -0,0 +1,9 @@
+// ataka bola dot notation
+
+const person = {
+    name: 'Utshab kuamr',
+    job: 'Freelanching'
+}
+console.log(person.name)
+//amra jodii third[] bracket ar moddo deya korta chay tahola string deya korta hoba
+console.log(person['job'])
