302f6ba73b501aa3f7e5906f5ae1884edbda7ce1
coder-utshab <ghoshutshob470@gmail.com>
2026-02-01 20:05:14 +0530

    up

---
+++ recap.js
@@ -0,0 +1,28 @@
+/**
+*1. var let const
+(var hocca amra reassign korta parbo)
+(const amra reassign kort a parbo nahh )
+
+2. defult parameter
+
+3. template string deya amra multiline kora falta pari ata khubi powerfull
+ */
+const messeage = `Hi, ${personalbar.name} has a: ${a} access to ${number [2]}`
+
+// 4. arrow function  important
+const square = x => x * x;
+const sum = (a, b) => a + b;
+
+
+//5. desstructuring and spread opearator
+//object neya jodii korii
+const { age, z, ...other } = { x: 2, y: 5, z: 3, name: 'ovujeet', age: 55 }
+
+//array ar disturing
+const [first, secoend, ...remaining] =['ram', 'sam', 'josu', 'modhu']
+
+//6. object.keys, object.values, object.entries
+
+// 7. for of used in array and string,
+
+//8. for in loop used in object
