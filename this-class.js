32ba308c2b7526103202dc8eba4d1cacfe657bcc
coder-utshab <ghoshutshob470@gmail.com>
2026-02-02 01:42:16 +0530

    js

--- this-class.js
+++ this-class.js
@@ -3,6 +3,19 @@ class Person{
         this.name = name;
         this.age = age;
     }
+    sleep(){
+        console.log(`Sleeping now ${this.name}`)
+    }
+    morning(){
+        console.log(`she is a student ${this.name}`)
+    }
 }
 const sajib = new Person('sajib Biswas',24)
 console.log(sajib)
+sajib.sleep();
+
+const dipto = new Person('parul university',26)
+dipto.sleep();
+
+const shuvo = new Person('she is a bangladesshi',32)
+shuvo.morning();
